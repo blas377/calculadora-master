7cb075324555cf426152f580cc6d86f720220a5c
blas377 <bruelop377@g.educaand.es>
2022-03-07 13:38:19 +0100

    funcion factorialbucle clase nombreAlumno

--- src/main/java/com/maksym/clases/FooNombreAlumno.java
+++ src/main/java/com/maksym/clases/FooNombreAlumno.java
@@ -2,4 +2,12 @@ package com.maksym.clases;
 
 public class FooNombreAlumno {
     // Clase nombre alumno - subido por blas
+
+    static void factorialbucle(int iNumero){
+        int iFactorial = 1;
+        for (int x=2;x<=iNumero;x++)
+        iFactorial = iFactorial * x;
+
+        System.out.println("El factorial del número " + Integer.toString(iNumero) +  " es " + Integer.toString(iFactorial));
+    }
 }
